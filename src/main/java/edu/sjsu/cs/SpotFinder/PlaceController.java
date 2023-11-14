37b543ef5dcd431cf0a8377401dd712470ff8cad
Matt E <mathew.estrada@sjsu.edu>
2023-11-13 20:27:26 -0800

    Matt E - API implementation / results page

--- src/main/java/edu/sjsu/cs/SpotFinder/PlaceController.java
+++ src/main/java/edu/sjsu/cs/SpotFinder/PlaceController.java
@@ -1,5 +1,7 @@
 package edu.sjsu.cs.SpotFinder;
 
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -10,10 +12,17 @@ public class PlaceController {
     @FXML private Label rating;
     @FXML private Label category;
 
-    public void displayDetails(Place place) {
-        name.setText(place.getName());
-        address.setText(place.getAddress());
-        rating.setText(String.format("%.2f stars", place.getRating()));
-        category.setText(place.getCategory());
+    public void displayDetails(List<Place> places) {
+        StringBuilder details = new StringBuilder();
+
+        for (Place place : places) {
+            details.append("Business Name: ").append(place.getName()).append("\n");
+            details.append("Address: ").append(place.getAddress()).append("\n");
+            details.append("Rating: ").append(String.format("%.2f stars", place.getRating())).append("\n");
+            details.append("Category: ").append(place.getCategory()).append("\n\n");
+        }
+
+        name.setText(details.toString());
     }
 }
+
